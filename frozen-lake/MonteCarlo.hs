0a73c80687c07f6b1b9fc6d4b4b388f8f4e54e75
augusto peres <augustoluisperes@gmail.com>
2020-12-21 20:19:59 +0000

    there is a bug somewhere in the montecarlo method

--- frozen-lake/MonteCarlo.hs
+++ frozen-lake/MonteCarlo.hs
@@ -30,6 +30,47 @@ agentFromGame game eps=
         , gen     = mkStdGen 1}
   where tiles = getTileList game
 
+policyToString :: Carlo AgentState -> (Int, Int) -> String
+policyToString agent (n, m) =
+  unlines $ map f [[(y, x) | x<-[0..n] ]| y<-[0..n]]
+  where f l = join $ map g l
+        g s = case policy agent M.!? s of
+          Nothing         -> " ERRO "
+          Just  D         -> "  D   "
+          Just  R         -> "  ->  "
+          Just  L         -> "  <-  "
+          Just  U         -> "  U   "
+          Just  KeepStill -> "  *   "
+
+learn :: FrozenLake -> Carlo AgentState -> Int -> Int -> Float -> Carlo AgentState
+learn g ag n l gamma =
+  go g ag M.empty n l gamma
+  where go _ agent _ 0 _  _ = agent
+        go game agent rets n' l' gamm =
+          let (newAgent, newReturns) = updateFromEpisode agent (reverse $ episode game agent l') gamm rets
+          in go game newAgent newReturns (n'-1) l' gamm
+
+-- | Input: An agent, An episode, a Float (gamma) denoting the learning rate
+-- Output: The agent with the updated qValues
+updateFromEpisode :: Carlo AgentState -> -- the agent
+                     [(AgentState, Action, Float)] -> -- the episode
+                     Float -> -- the learning rate
+                     M.Map (AgentState, Action) [Float] -> -- the previous returns
+                     (Carlo AgentState, M.Map (AgentState, Action) [Float])
+updateFromEpisode ag ep gam rets =
+  go ag ep rets 0 gam
+  where go agent [] returns _ _ = (makeGreedy $ changeQValues agent returns, returns)
+        go agent ((s, a, r):xs) returns ret gamma =
+          if (s, a) `elem` [(s', a') | (s', a', _)<-xs]
+          then go agent xs returns (gamma*ret + r) gamma
+          else go agent xs (M.insertWith (++) (s, a) [gamma*ret + r] returns) (gamma*ret + r) gamma
+
+changeQValues :: Carlo AgentState -> M.Map (AgentState, Action) [Float] -> Carlo AgentState
+changeQValues agent returns =
+  agent { qValues = newQValues }
+  where newQValues = foldr (\x y -> M.updateWithKey f x y) (qValues agent) (M.keys returns)
+        f key _ = do {l <- returns M.!? key ; return $  sum l / (fromIntegral $ length l) }
+
 -- | Input: Initial game, Initial agent, number of moves
 --   Output: An episode in the from state action reward
 -- The episode is returned as [(s0, a0, r1),..., (sn-1, an-1, rn)].
@@ -58,7 +99,9 @@ takeAction s = do
   agent <- get
   let (val, g) = sampleState ( uniform 0 1) (gen agent)
       action   = if val >= epsilon agent
-                 then fromJust $ policy agent M.!? s
+                 then case policy agent M.!? s of
+                        Nothing -> choice (actions agent) g
+                        Just a  -> a
                  else choice (actions agent) g
   put agent { gen = g }
   return action
